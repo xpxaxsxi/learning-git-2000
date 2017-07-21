e254401474a726aca519e3d4d340dc9c39ffa561
A Bored Git Newbie <xxxxisap2@gmail.com>
2017-07-21 22:59:56 +0300

    testing

--- alpha.pl
+++ alpha.pl
@@ -3,4 +3,6 @@ a.
 q.
 
 % I don't like git, I'm sure it is essential for collaborated programming.
-Trying to stir up interest to git from GCE cloudshell
+testline('Trying to stir up interest to git from GCE cloudshell').
+testline(' The cloudshell is a temporary file system with Google Compute Engine').
+
