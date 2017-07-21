c457ac77dfed96142db0e8ce69a3dfa3cd1d6995
A Bored Git Newbie <xxxxisap2@gmail.com>
2017-07-21 23:04:38 +0300

    Having some interest, I can feel that

--- alpha.pl
+++ alpha.pl
@@ -5,4 +5,5 @@ q.
 % I don't like git, I'm sure it is essential for collaborated programming.
 testline('Trying to stir up interest to git from GCE cloudshell').
 testline(' The cloudshell is a temporary file system with Google Compute Engine').
+testline(' I thought the editor would be vi but it is nano').
 
